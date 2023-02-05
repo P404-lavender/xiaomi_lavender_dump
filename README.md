781b8d4cbdd9e92796b023902015ed01f8eb4d66
xenxynon <flynryder427@gmail.com>
2023-02-05 04:39:18 +0000

    Add extras for aospa_lavender-userdebug 13 TKQ1.221223.001 eng.mayur.20230201.164939 release-keys
    Signed-off-by: xenxynon <flynryder427@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## aospa_lavender-userdebug 13 TKQ1.221223.001 eng.mayur.20230201.164939 release-keys
+- Manufacturer: xiaomi
+- Platform: sdm660
+- Codename: lavender
+- Brand: Xiaomi
+- Flavor: aospa_lavender-userdebug
+- Release Version: 13
+- Id: TKQ1.221223.001
+- Incremental: eng.mayur.20230201.164939
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-US
+- Screen Density: undefined
+- Fingerprint: Xiaomi/aospa_lavender/lavender:13/TKQ1.221223.001/mayur02011647:userdebug/release-keys
+- OTA version: 
+- Branch: aospa_lavender-userdebug-13-TKQ1.221223.001-eng.mayur.20230201.164939-release-keys
+- Repo: xiaomi_lavender_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cat system/system/product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> system/system/product/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f system/system/product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
+cat system/system/product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null >> system/system/product/app/LatinImeGoogle/LatinImeGoogle.apk
+rm -f system/system/product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null
+cat system/system/product/app/Maps/Maps.apk.* 2>/dev/null >> system/system/product/app/Maps/Maps.apk
+rm -f system/system/product/app/Maps/Maps.apk.* 2>/dev/null
+cat system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> system/system/product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat system/system/product/app/Gmail2/Gmail2.apk.* 2>/dev/null >> system/system/product/app/Gmail2/Gmail2.apk
+rm -f system/system/product/app/Gmail2/Gmail2.apk.* 2>/dev/null
+cat system/system/product/app/Messages/Messages.apk.* 2>/dev/null >> system/system/product/app/Messages/Messages.apk
+rm -f system/system/product/app/Messages/Messages.apk.* 2>/dev/null
+cat system/system/product/app/GoogleCameraGo/GoogleCameraGo.apk.* 2>/dev/null >> system/system/product/app/GoogleCameraGo/GoogleCameraGo.apk
+rm -f system/system/product/app/GoogleCameraGo/GoogleCameraGo.apk.* 2>/dev/null
+cat system/system/product/app/YouTube/YouTube.apk.* 2>/dev/null >> system/system/product/app/YouTube/YouTube.apk
+rm -f system/system/product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat system/system/product/app/Photos/Photos.apk.* 2>/dev/null >> system/system/product/app/Photos/Photos.apk
+rm -f system/system/product/app/Photos/Photos.apk.* 2>/dev/null
+cat system/system/product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> system/system/product/priv-app/GmsCore/GmsCore.apk
+rm -f system/system/product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat system/system/product/priv-app/DevicePersonalizationPrebuiltPixel2020/DevicePersonalizationPrebuiltPixel2020.apk.* 2>/dev/null >> system/system/product/priv-app/DevicePersonalizationPrebuiltPixel2020/DevicePersonalizationPrebuiltPixel2020.apk
+rm -f system/system/product/priv-app/DevicePersonalizationPrebuiltPixel2020/DevicePersonalizationPrebuiltPixel2020.apk.* 2>/dev/null
+cat system/system/product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> system/system/product/priv-app/Velvet/Velvet.apk
+rm -f system/system/product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/system_ext/priv-app/Settings/Settings.apk
+rm -f system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
